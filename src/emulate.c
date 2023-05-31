f753bbc6707ba0f8ed89a1fa52b58352359ea374
km2122 <km2122@ic.ac.uk>
2023-05-31 14:57:28 +0100

    Conformed code to Google Style

--- src/emulate.c
+++ src/emulate.c
@@ -1,29 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main( int argc, char **argv ) {
-  if( argc != 2 ) {
-    fprintf( stderr, "Usage: cat filename!\n" );
-    exit(1);
-  }
-  show_file( argv[1] );
-  return 0;
-}
-
-void show_file( char *filename ) {
+void show_file(char *filename) {
   // open the file
-  FILE *fp = fopen( filename, "r" );
-  if( fp == NULL ) {
-    fprintf( stderr, "cat: cannot open %s\n", filename );
+  FILE *fp = fopen(filename, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "cat: cannot open %s\n", filename);
     exit(1);
   }
 
   // Now read the contents of the open file and print them out
   int ch;
-  while( (ch = getc(fp)) != EOF ) {
-    putchar( ch );
+  while ((ch = getc(fp)) != EOF) {
+    putchar(ch);
     printf("\n");
-    }
-    // close the file
-    fclose(fp);
+  }
+  // close the file
+  fclose(fp);
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: cat filename!\n");
+    exit(1);
+  }
+  show_file(argv[1]);
+  return 0;
 }
