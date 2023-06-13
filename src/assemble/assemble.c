db8b9ee0373d2976eef05172170cb78086d3034d
km2122 <km2122@ic.ac.uk>
2023-06-13 15:41:49 +0100

    changed cmake file and altered assemble.h

--- src/assemble/assemble.c
+++ src/assemble/assemble.c
@@ -1,3 +1,4 @@
+#include "assemble.h"
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
--- src/assemble/assemble.h
+++ src/assemble/assemble.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include "parser/parser.h"
 
-typedef uint32_t (*MACHINE_CODE_CONSTRUCTOR)(Parser_Tree);
+typedef uint32_t (MACHINE_CODE_CONSTRUCTOR)(Parser_Tree);
 
 MACHINE_CODE_CONSTRUCTOR register_dp_assembler;
 MACHINE_CODE_CONSTRUCTOR immediate_dp_assembler;
