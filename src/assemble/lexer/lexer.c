2aa21a7ec4dc4ab40a0e2d7e69e011ef4bc9102c
Ethan Hosier <ethanjhosier@gmail.com>
2023-06-14 18:43:14 +0100

    added logic to check for instruction token

--- src/assemble/lexer/lexer.c
+++ src/assemble/lexer/lexer.c
@@ -2,15 +2,51 @@
 #include <string.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <assert.h>
 
 #include "lexer.h"
 #include "../../ArrayList.h"
 
 #include "../../BinarySearchTree.h"
 
+#define NEW(t) malloc(sizeof(t))
+
 static BinarySearchTree instructionsBST;
 
-static void ()
+static void initialiseInstructionsBST(){
+    const char* instructions[] = {
+            "adds",
+            "sub",
+            "cmp",
+            "cmn",
+            "neg",
+            "and",
+            "bic",
+            "eor",
+            "eon",
+            "orr",
+            "orn",
+            "tst",
+            "mvn",
+            "mov",
+            "movn",
+            "movk",
+            "movz",
+            "madd",
+            "msub",
+            "mul",
+            "mneg",
+            "b",
+            "br",
+            "b.cond",
+            "ldr",
+            "str"
+    };
+
+    for (int i = 0; i < sizeof(instructions) / sizeof (instructions[0]); i++) {
+        add_strbst(instructionsBST, instructions[i]);
+    }
+}
 
 static void print_arrayList_element(void *element){
     fprintf(stdout, "willy");
@@ -19,23 +55,22 @@ static void print_arrayList_element(void *element){
 }
 
 
-static Token *checkIfInstruction(char *str){
-    int RANGE = INT_MAX;
-
-
-
-}
-
-
 static Token *string_to_token(char *str){
-    int RANGE = INT_MAX;
-
-
     fprintf(stdout, "%s\n",str);
 
+    if (in_strbst(instructionsBST,str)){
+        Token *t = NEW(Token);
+        assert(t != NULL);
 
+        InstructionToken *instructionToken = NEW(Token);
+        assert(instructionToken != NULL);
 
+        instructionToken->instruction = str;
+        t->instructionToken = *instructionToken;
+        t->type = TOKEN_TYPE_INSTRUCTION;
+    }
 
+    
 
     return NULL;
 }
@@ -43,6 +78,8 @@ static Token *string_to_token(char *str){
 
 ArrayList *tokenize(char *line)
 {
+    initialiseInstructionsBST();
+
     ArrayList *tokens = create_ArrayList(print_arrayList_element, &free);
     char *tokenStr;
 
