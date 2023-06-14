bed793964ab87acdced27f4575506b492725a363
km2122 <km2122@ic.ac.uk>
2023-06-14 19:05:57 +0100

    Renamed record to immediateToken

--- src/assemble/lexer/lexer.h
+++ src/assemble/lexer/lexer.h
@@ -43,7 +43,7 @@ typedef struct {
     union {
         InstructionToken instructionToken;
         RegisterToken registerToken;
-        ImmediateToken constantToken;
+        ImmediateToken immediateToken;
         LabelToken labelToken;
         DirectiveToken directiveToken;
     };
