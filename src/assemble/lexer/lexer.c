f5dd2bc78676b4325a38d0be836cd7b61353752f
Ethan Hosier <ethanjhosier@gmail.com>
2023-06-14 21:12:03 +0100

    The basic idea of how to handle address code instruction, 99% will be errors with repeated code + pointers instead of values xx

--- src/assemble/lexer/lexer.c
+++ src/assemble/lexer/lexer.c
@@ -15,111 +15,178 @@
 static BinarySearchTree instructionsBST;
 
 static void initialiseInstructionsBST() {
-  const char *instructions[] = {
-          "adds",
-          "sub",
-          "cmp",
-          "cmn",
-          "neg",
-          "and",
-          "bic",
-          "eor",
-          "eon",
-          "orr",
-          "orn",
-          "tst",
-          "mvn",
-          "mov",
-          "movn",
-          "movk",
-          "movz",
-          "madd",
-          "msub",
-          "mul",
-          "mneg",
-          "b",
-          "br",
-          "b.cond",
-          "ldr",
-          "str"
-  };
+    const char *instructions[] = {
+            "adds",
+            "sub",
+            "cmp",
+            "cmn",
+            "neg",
+            "and",
+            "bic",
+            "eor",
+            "eon",
+            "orr",
+            "orn",
+            "tst",
+            "mvn",
+            "mov",
+            "movn",
+            "movk",
+            "movz",
+            "madd",
+            "msub",
+            "mul",
+            "mneg",
+            "b",
+            "br",
+            "b.cond",
+            "ldr",
+            "str"
+    };
 
-  for (int i = 0; i < sizeof(instructions) / sizeof(instructions[0]); i++) {
-    add_strbst(instructionsBST, instructions[i]);
-  }
+    for (int i = 0; i < sizeof(instructions) / sizeof(instructions[0]); i++) {
+        add_strbst(instructionsBST, instructions[i]);
+    }
 }
 
 static void print_arrayList_element(void *element) {
-  fprintf(stdout, "willy");
-
-
+    fprintf(stdout, "willy");
 }
 
 
-static Token *string_to_token(char *str) {
-  fprintf(stdout, "%s\n", str);
+static Token string_to_token(char *str) {
+    fprintf(stdout, "%s\n", str);
 
-  Token *t = NEW(Token);
-  assert(t != NULL);
+    Token t = NEW(struct Token);
+    assert(t != NULL);
 
-  if (strcmp(str, ".int") == 0) {
-    t->type = TOKEN_TYPE_DOT_INT;
-  }
+    if (strcmp(str, ".int") == 0) {
+        t->type = TOKEN_TYPE_DOT_INT;
+    }
 
-    //check for instruciton token
-  else if (in_strbst(instructionsBST, str)) {
-    InstructionToken *instructionToken = NEW(InstructionToken);
-    assert(instructionToken != NULL);
+        //check for instruciton token
+    else if (in_strbst(instructionsBST, str)) {
+        InstructionToken *instructionToken = NEW(InstructionToken);
+        assert(instructionToken != NULL);
 
-    instructionToken->instruction = str;
-    t->instructionToken = *instructionToken;
-    t->type = TOKEN_TYPE_INSTRUCTION;
-  }
+        instructionToken->instruction = str;
+        t->instructionToken = *instructionToken;
+        t->type = TOKEN_TYPE_INSTRUCTION;
+    }
 
-    //check for immediate token
-  else if (str[0] == '#') {
-    ImmediateToken *immediateToken = NEW (ImmediateToken);
-    assert(immediateToken != NULL);
+        //check for immediate token
+    else if (str[0] == '#') {
+        ImmediateToken *immediateToken = NEW (ImmediateToken);
+        assert(immediateToken != NULL);
 
-    immediateToken->value = (uint32_t) atoi(str);
-    t->immediateToken = *immediateToken;
-    t->type = TOKEN_TYPE_IMMEDIATE;
-  }
-
-
-
-    //treat as a label
-  else {
-    LabelToken *labelToken = NEW(LabelToken);
-
-  }
+        immediateToken->value = (uint32_t) atoi(str);
+        t->immediateToken = *immediateToken;
+        t->type = TOKEN_TYPE_IMMEDIATE;
+    }
 
 
-  return t;
+
+        //treat as a label
+    else {
+        LabelToken *labelToken = NEW(LabelToken);
+        assert(labelToken != NULL);
+
+        labelToken->label = str;
+        t->labelToken = *labelToken;
+        t->type = TOKEN_TYPE_LABEL;
+    }
+
+
+    return t;
 
 }
 
+AddressCodePossibilities check_if_address_code(char *str) {
+    if (str[0] != '[') {
+        return ADDRESS_CODE_POSSIBILITIES_NOT;
+    }
+
+    int length = strlen(str);
+
+    if (str[length - 1] == ']' || str[length - 1] == '!') {
+        return ADDRESS_CODE_POSSIBILITIES_ONE;
+    }
+
+    return ADDRESS_CODE_POSSIBILITIES_TWO;
+}
 
 ArrayList *tokenize(char *line) {
-  initialiseInstructionsBST();
+    initialiseInstructionsBST();
 
-  ArrayList *tokens = create_ArrayList(print_arrayList_element, &free);
-  char *tokenStr;
+    ArrayList *tokens = create_ArrayList(print_arrayList_element, &free);
+    char *tokenStr;
+
+    tokenStr = strtok(line, " ");
+    while (tokenStr != NULL) {
+        // Create a copy of the token and add it to the tokens list
+        char *tokenStrCopy = strdup(tokenStr);
+
+        /*
+        if (check_if_address_code(tokenStrCopy) == ADDRESS_CODE_POSSIBILITIES_NOT) {
+            Token t = string_to_token(tokenStrCopy);
+            add_ArrayList_element(tokens, t);
+            tokenStr = strtok(NULL, " ,");
+        } else {
+            tokenStr = strtok(NULL, " ,");
+        }
+         */
+
+        switch (check_if_address_code(tokenStrCopy)) {
+            case ADDRESS_CODE_POSSIBILITIES_NOT: {
+                Token t = string_to_token(tokenStrCopy);
+                add_ArrayList_element(tokens, t);
+                break;
+            }
+            case ADDRESS_CODE_POSSIBILITIES_ONE:{
+                Token t = NEW(struct Token);
+                assert(t != NULL);
+
+                AddressCodeToken *act = NEW (AddressCodeToken);
+                assert(act != NULL);
+
+                Token tInside = string_to_token(strtok(tokenStrCopy," []!"));
+
+                act-> t1 = tInside;
+
+                t->type = TOKEN_ADDRESS_CODE;
+                t->addressToken = *act;
 
 
-  tokenStr = strtok(line, " ");
-  while (tokenStr != NULL) {
-    // Create a copy of the token and add it to the tokens list
-    char *tokenStrCopy = strdup(tokenStr);
-    Token *t = string_to_token(tokenStrCopy);
-    add_ArrayList_element(tokens, t);
+                break;
+            }
+
+            case ADDRESS_CODE_POSSIBILITIES_TWO: {
+                Token t = NEW (struct Token);
+                assert(t != NULL);
+
+                AddressCodeToken *act = NEW (AddressCodeToken);
+                assert(act != NULL);
+
+                Token t1 = string_to_token(strtok(tokenStrCopy," []!"));
+                tokenStr = strtok(NULL, " ,");
+                char *tokenStrCopy = strdup(tokenStr);
+                Token t2 = string_to_token(strtok(tokenStrCopy," []!"));
+
+                act->t1 = t1;
+                act->pT2 = t2;
 
 
-    tokenStr = strtok(NULL, " ,");
-  }
+                t->type = TOKEN_ADDRESS_CODE;
+                t->addressToken = *act;
 
+            }
+        }
 
-  return tokens;
+        tokenStr = strtok(NULL, " ,");
+
+    }
+
+    return tokens;
 
 
 }
--- src/assemble/lexer/lexer.h
+++ src/assemble/lexer/lexer.h
@@ -5,6 +5,20 @@
 #include <stdint.h>
 #include "../../ArrayList.h"
 
+typedef enum {
+    TOKEN_TYPE_INSTRUCTION,
+    TOKEN_TYPE_IMMEDIATE,
+    TOKEN_TYPE_REGISTER,
+    TOKEN_TYPE_LABEL,
+    TOKEN_TYPE_DOT_INT,
+    TOKEN_ADDRESS_CODE,
+} TokenType;
+
+typedef enum{
+    ADDRESS_CODE_POSSIBILITIES_NOT,
+    ADDRESS_CODE_POSSIBILITIES_ONE,
+    ADDRESS_CODE_POSSIBILITIES_TWO
+} AddressCodePossibilities;
 
 typedef struct {
     char *instruction;
@@ -24,25 +38,27 @@ typedef struct {
     char *label;
 } LabelToken;
 
+struct Token;
+typedef struct Token *Token;
 
-typedef enum {
-    TOKEN_TYPE_INSTRUCTION,
-    TOKEN_TYPE_IMMEDIATE,
-    TOKEN_TYPE_REGISTER,
-    TOKEN_TYPE_LABEL,
-    TOKEN_TYPE_DOT_INT
-} TokenType;
+typedef struct{
+    TokenType type;
+    Token t1;
+    Token pT2;
+} AddressCodeToken;
 
 
-typedef struct {
+
+struct Token {
     TokenType type;
     union {
         InstructionToken instructionToken;
         RegisterToken registerToken;
         ImmediateToken immediateToken;
         LabelToken labelToken;
+        AddressCodeToken addressToken;
     };
-} Token;
+};
 
 
 extern ArrayList *tokenize(char *line);
