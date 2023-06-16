0f02efa916b73ccd760b92096aa27863851341c9
km2122 <km2122@ic.ac.uk>
2023-06-16 01:47:39 +0100

    Fixed a segfault modified assemble.c structure

--- src/assemble/assemble.c
+++ src/assemble/assemble.c
@@ -3,14 +3,21 @@
 #include <printf.h>
 
 int main(int argc, char **argv) {
-    char str[] = "ldr x20, [x5, #8]";
-    ArrayList *tokens = tokenize(str);
-    print_ArrayList_elements(tokens);
+//  char str[] = "ldr x20, [x5, #8]";
+//  ArrayList *tokens = tokenize(str);
+//  print_ArrayList_elements(tokens);
 
-  char **lines = NULL; //readLines(file_name);
+  char line[] = "add x0, x0, x0";
+  int number_of_lines = 1;
+  char **lines =
+      malloc(number_of_lines * sizeof(char *)); //readLines(file_name);
+  lines[0] = line;
   ArrayList *token_lines = create_ArrayList(NULL, &free_ArrayList);
-  for (int i = 0; i < sizeof(lines); i++) // maybe works
-    add_ArrayList_element(token_lines, tokenize(lines[i]));
+  for (int i = 0; i < number_of_lines; i++) {// maybe works
+    ArrayList *tokens = tokenize(lines[i]);
+    add_ArrayList_element(token_lines, tokens);
+  }
+  free(lines);
   TreeMap *label_identifiers = first_pass(token_lines);
   ArrayList *trees = second_pass(token_lines, label_identifiers);
   free_ArrayList(token_lines);
